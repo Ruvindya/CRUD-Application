bb065b9fb65ae743f8a2d6e67ee3660558b67a28
Ruvindya <2019e117@eng.jfn.ac.lk>
2024-01-31 02:12:31 +0530

    image upload with multer

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
 import express from 'express';
 import mysql from "mysql";
+import cors from "cors";
+import multer from 'multer';
+import path from 'path';
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: './src/profilePicture',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+    
+
+const upload = multer({
+    storage: storage
+});
+
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -9,12 +27,12 @@ const db = mysql.createConnection({
     database:"crud_application"
 })
 
-app.use(express.json())
 
 app.get("/", (req, res)=>{
     res.json("Hello this is backend")
 })
-app.get("/customer", (req,res)=>{
+
+app.get("/customers", (req,res)=>{
     const q = "SELECT * FROM customer"
     db.query(q,(err,data)=>{
         if(err) return res.json(err)
@@ -22,7 +40,16 @@ app.get("/customer", (req,res)=>{
     })
 })
 
-app.post("/customer",(req,res)=>{
+app.get("/customers/:id", (req,res)=>{
+    const customerID = req.params.id;
+    const q = "SELECT * FROM customer WHERE id = ?"
+    db.query(q,[customerID],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/customer",upload.single('profilePicture') , (req,res)=>{
     const q = "INSERT INTO customer (`firstName`, `lastName`, `age`,`address`,`description`,`profilePicture`) VALUES (?)";
     const values = [
         req.body.firstName,
@@ -30,15 +57,45 @@ app.post("/customer",(req,res)=>{
         req.body.age,
         req.body.address,
         req.body.description,
-        req.body.profilePicture
+        req.file.fieldname
     ];
 
     db.query(q,[values], (err, data)=> {
         if (err) return res.json(err);
-        return res.json("customer data entered successfully");
+        return res.json(data);
+    });
+});
+
+
+app.delete("/customer/:id", (req,res)=>{
+    const customerID = req.params.id;
+    const q = "DELETE FROM customer WHERE id = ?";
+
+    db.query(q,[customerID], (err,data)=>{
+        if (err) return res.json(err);
+        return res.json("customer data deleted successfully");
+    });
+});
+
+app.put("/customer/:id", (req,res)=>{
+    const customerID = req.params.id;
+    const q = "UPDATE customer SET `firstName` = ?, `lastName` = ?, `age` = ?,`address` = ?,`description` = ?,`profilePicture` = ? WHERE id = ?";
+
+    const values = [
+        req.body.firstName,
+        req.body.lastName,
+        req.body.age,
+        req.body.address,
+        req.body.description,
+        req.body.profilePicture,
+    ]
+
+    db.query(q,[...values, customerID], (err,data)=>{
+        if (err) return res.json(err);
+        return res.json("customer data updated successfully");
     });
 });
 
 app.listen(8800, ()=>{
-    console.log("Connected to backend server!")
+    console.log("Connected to backend on PORT 8800!")
 })
